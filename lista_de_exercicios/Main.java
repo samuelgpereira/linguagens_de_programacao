6b8fad5926756299491354e35d6e41ce62907bc0
samuelgpereira <samuel.gpereira2@gmail.com>
2026-09-16 15:08:42 -0300

    Lista de exercicios

---
+++ lista_de_exercicios/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        //1 - Maior de 2 numeros
+        //Crie 2 variáveis inteiras e mostre qual delas é maior
+        int numero 1 = 10, numero 2 = 20;
+        if (numero 1 > numero 2){
+            System.out.println("O maior valor eh: " + numero 1);
+        }else{
+            System.out.println("O maior valor eh " + numero 2);
+        }
+
+    }
+}
